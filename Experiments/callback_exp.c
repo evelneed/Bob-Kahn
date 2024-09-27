eb565464b44a80e1c8982dddfc3296314fc84179
ohk99 <oliverkendall@MacBook-Pro-126.local>
2024-09-27 15:02:50 -0600

    updated callback to address time tick differencees between rising and falling edge, still does not fully work.

--- Experiments/callback_exp.c
+++ Experiments/callback_exp.c
@@ -8,32 +8,54 @@
 uint32_t previous_tick = 0;
 uint32_t first_tick = 0;
 uint32_t base_time = 0;
-int first_transition = 1;
+uint32_t rising_base_time = 0;
+uint32_t falling_base_time = 0;
+int first_falling_edge = 1;
+int first_rising_edge = 1;
 int w = 0;
 
 
-//TODO figure out the issues with percent error as it is getting caught on that with the previous tick not changin and so the time difference gets larger and larger
-//issue is mainly with previous tick
-//check logic again and error lenience with the tick checking 
-
-
+//TODO program must start high
+//callback for rising edges 
 void call_back(int pi, unsigned gpio, unsigned level, uint32_t tick){
-	//setting base comparison time with header
-	if(base_time == 0 && first_transition == 0) {
-		base_time = tick - first_tick;
+	printf("loop# %d level: %x \n", w,level);
+
+	//get base_time with header for rising and falling edges 
+	//setting base time for rising edge comparison time with header
+	if(rising_base_time == 0 && first_rising_edge == 0 && level == 0) {
+		rising_base_time = tick - first_tick;
 		previous_tick = first_tick;
-		//printf("base_time: %x  tick: %x  first_tick: %x \n", base_time,tick,first_tick);
+		printf( "setting rising base: %li loop #: %d \n",rising_base_time,w);
+		//printf("base_time: %li  tick: %li  first_tick: %li \n", base_time,tick,first_tick);
+	}
+
+	//setting the base_time for falling edge
+	if(first_falling_edge && level == 1 && first_rising_edge == 0){
+		falling_base_time = tick-previous_tick;
+		first_falling_edge = 0; 
+		printf("setting falling base time: %li loop#: %d \n", falling_base_time, w);
 	}
 
 	//edge case for start
-	if (first_transition){
+	if (first_rising_edge){
 		first_tick = tick;
-		first_transition = 0;
+		first_rising_edge = 0;
+	}
+
+	//set base time
+	if (level == 0) {
+		base_time = rising_base_time;
+	}
+	else if (level == 1) {
+		base_time = falling_base_time;
+	}
+	else {
+		printf("error");
 	}
 
 	//get current tick comparison time
 	uint32_t current_time = tick - previous_tick;
-	//printf("current time: %x, tick: %x, prev_tick %x", current_time, tick, previous_tick);
+	//printf("current time: %li, tick: %li, prev_tick %li \n", current_time, tick, previous_tick);
 
 	//check current time vs base time
 	double percent_error = fabs(((double)base_time - current_time) / base_time) * 100.0;
@@ -43,35 +65,35 @@ void call_back(int pi, unsigned gpio, unsigned level, uint32_t tick){
 	//then see if it is within 25% error of original time if so record it, if not than it is in between change between re[eat numbers
 	if ((previous_tick != 0) && (base_time != 0)){
 		if (percent_error > 25){ 
-			printf(" incorrect transition percent error: %.2f%%  tick: %x previous_tick: %x base_time: %x curremt_time: %x \n", percent_error,tick,previous_tick,base_time,current_time);
+			printf(" incorrect transition percent error: %.2f%%  tick: %li curremt_time: %li \n", percent_error,tick ,current_time);
+			printf(" rising base time: %li falling base time: %li base time: %li ", rising_base_time, falling_base_time, base_time);
 		}
 		else{
-			printf("correct transition  level: %u percent_error: %.2f%%  \n",level,percent_error);
-			//previous_tick = tick;
+			printf("correct transition  level: %u percent_error: %.2f%% rising_base_time: %li falling-base_time: %li  \n",level,percent_error,rising_base_time,falling_base_time);
+			previous_tick = tick;
 			//set the base time to the previous time to avoid time drift
 			//base_time = current_time;
 		}		
 	}
-	previous_tick = tick;
+	//previous_tick = tick;
 	w++;
-		
 }
-void main() {	
+void main() {
 	pigpio_start(0,0);
-	//initialize callback on port for recv
-	int bud = callback(0, 20, EITHER_EDGE, call_back);
-	
-	//tester message with 10 as the header and the rest being a normal message
-	int tester_mess[] = {1,0,1,0,0,1,1,0,1,1};
-	//size of tester array
+	//initialize rising and falling callback on port 4 recv
+	int rising_callback = callback(0, 20, EITHER_EDGE, call_back);
+	//the program must start high
+	//tester message the header is 011001 
+	int tester_mess[] = {0,1,1,0,0,1,1,0,1,0};
+	//size of tester mess
 	int size = sizeof(tester_mess)/sizeof(tester_mess[0]);
-
-	//loop through and write each to the gpio
-	for (int i = 0; i <size; i++) {
+	//TODO dont rely on setting the state below
+	gpio_write(0,27,0);
+	//loop through and write to gpio
+	for (int i = 0; i < size; i++) {
 		gpio_write(0,27,tester_mess[i]);
 		sleep(.0001);
 	}
+	
 	time_sleep(3);
-	}
-	 
-
+}
