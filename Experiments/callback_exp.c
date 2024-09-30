3f08f74a090874f9b73b81e01476adff4e377011
ohk99 <oliverkendall@MacBook-Pro-126.local>
2024-09-29 20:46:10 -0600

    completed callback experiment added tail and header recognition working with any input hurray

--- Experiments/callback_exp.c
+++ Experiments/callback_exp.c
@@ -6,18 +6,20 @@
 
 //global variables for tick comparison
 uint32_t previous_tick = 0;
-uint32_t first_tick = 0;
 uint32_t base_time = 0;
 uint32_t rising_base_time = 0;
 uint32_t falling_base_time = 0;
 int first_falling_edge = 1;
 int first_rising_edge = 1;
+//not first edge change identifier  
+int not_first = 1;
 int w = 0;
 int i = 0;
-int results [14];
+//dynamically allocating memory for the messaging data
+int results[1000];
+int results_size = 0;
 
 
-//TODO program must start high
 //callback for rising edges 
 void call_back(int pi, unsigned gpio, unsigned level, uint32_t tick){
 	printf("loop# %d level: %x \n", w,level);
@@ -25,8 +27,8 @@ void call_back(int pi, unsigned gpio, unsigned level, uint32_t tick){
 	//get base_time with header for rising and falling edges 
 	//setting base time for rising edge comparison time with header
 	if(rising_base_time == 0 && first_rising_edge == 0 && level == 0) {
-		rising_base_time = tick - first_tick;
-		previous_tick = first_tick;
+		rising_base_time = tick - previous_tick;
+		not_first = 0;
 		//printf( "setting rising base: %li loop #: %d \n",rising_base_time,w);
 		//printf("base_time: %li  tick: %li  first_tick: %li \n", base_time,tick,first_tick);
 	}
@@ -35,13 +37,15 @@ void call_back(int pi, unsigned gpio, unsigned level, uint32_t tick){
 	if(first_falling_edge && level == 1 && first_rising_edge == 0){
 		falling_base_time = tick-previous_tick;
 		first_falling_edge = 0; 
+		not_first = 0;
 		//printf("setting falling base time: %li loop#: %d \n", falling_base_time, w);
 	}
 
 	//edge case for start
-	if (first_rising_edge && level == 1){
-		first_tick = tick;
+	if (first_rising_edge && level == 0){
+		previous_tick = tick;
 		first_rising_edge = 0;
+
 	}
 
 	//set base time
@@ -65,40 +69,80 @@ void call_back(int pi, unsigned gpio, unsigned level, uint32_t tick){
 	
 	//check its not the first or second state change
 	//then see if it is within 25% error of original time if so record it, if not than it is in between change between re[eat numbers
-	if ((previous_tick != 0) && (base_time != 0)){
-		if (percent_error > 25){ 
+	if ((previous_tick != 0) && (base_time != 0) && (not_first == 0)){
+		//tail recognition 
+		if (percent_error > 300) {
+			printf("end of message");
+		}
+		else if (percent_error > 25){ 
 			printf(" incorrect transition percent error: %.2f%%  base_time: %li current_time: %li level: %u \n", percent_error,base_time,current_time,level);
 		}
 		else{
 			printf("correct transition  level: %u percent_error: %.2f%% rising_base_time: %li falling-base_time: %li  \n",level,percent_error,rising_base_time,falling_base_time);
 			previous_tick = tick;
-			//store values in an array
-			results[i] = level;
-			i++;
-			//set the base time to the previous time to avoid time drift
-			//base_time = current_time;
+
+
+			//store values
+			if (results_size < 1000) {
+				results[results_size] = level;
+				results_size++;
+			}
+			else {
+				printf("maximum bits reached");
+			}	
 		}		
 	}
-	//previous_tick = tick;
 	w++;
+}
+
+void process_results() {
+	if (results_size < 2) {
+		printf("not enough bits to process");
+		return;
+	}
+
+	//shift the array to remove the header bits
+	for (int j = 2; j < results_size; j++) {
+		results[j - 2] = results[j];
+	}
+	//reduce size by 2 for the first two and then back for the tail
+	results_size -= 2;
+
+	//flip the bits to display original message (0 to 1s, 1s to 0's)
+	for (int j = 0; j <results_size; j++) {
+		results[j] = results[j] == 0 ? 1 : 0;
+	}
+
+	//display results
+	printf("message received: \n");
+	for (int j = 0; j < results_size; j++) {
+		printf("%d,", results[j]);
+	}
 }
 void main() {
 	pigpio_start(0,0);
 	//initialize rising and falling callback on port 4 recv
-	int rising_callback = callback(0, 20, EITHER_EDGE, call_back);
-	//the program must start high
-	//tester message the header is 011001 
-	int tester_mess[] = {0,1,1,0,0,1,0,1,1,0};
-	//size of tester mess
-	int size = sizeof(tester_mess)/sizeof(tester_mess[0]);
-	//loop through and write to gpio
-	/*for (int i = 0; i < size; i++) {
-		gpio_write(0,27,tester_mess[i]);
-		sleep(.000001);
-	}
-	*/
-	time_sleep(5);
-	for (int z = 0; z <14; z++) {
-		printf("%d ,",results[z]);
-	}
+	int call_back_one = callback(0, 20, EITHER_EDGE, call_back);
+	//header
+/*	
+	gpio_write(0,27,1);
+	sleep(.5);
+	gpio_write(0,27,0);
+	sleep(.5);
+	gpio_write(0,27,1);
+	sleep(.5);
+	gpio_write(0,27,0);
+	sleep(.5);
+	//message
+	gpio_write(0,27,1);
+	sleep(.5);
+	gpio_write(0,27,0);
+	sleep(.5);
+	//tail could be a high held for 4 time signatures
+	gpio_write(0,27,1);
+	sleep(1);
+	gpio_write(0,27,0);
+*/	
+	time_sleep(10);
+	process_results();
 }
